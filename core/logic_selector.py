1de1d77213a07c1e801e71630ed9f18a8f1359f2
Khaled-Rabee23 <khaledrabee303@gmail.com>
2026-05-09 08:35:05 -0400

    Fix: selection logic now handles empty inputs

--- core/logic_selector.py
+++ core/logic_selector.py
@@ -12,15 +12,23 @@ class RandomSelector:
         # BUG: This doesn't remove leading/trailing spaces from items.
         # Hint: Look into the .strip() method.
         self.options = input_string.split(",")
+        self.options = [item.strip() for item in self.options] # Added the .strip() method to remove
+        # the leading/trailing spaces from the items and assigning it back to the orignal list
 
     def select_items(self, count):
         """
         Randomly selects 'count' number of items from the list.
         """
         # TODO: Add a check to ensure 'count' is not greater than len(self.options)
-        
+        if count > len(self.options):
+            return "Invaild number of options"
+          
         # BUG: This currently only returns the first 'count' items, not random ones.
         # Hint: Use random.sample() for unique random selections.
-        selected = self.options[:count] 
+
+        # selected = self.options[:count] // The wrong one
+        selected = random.sample(self.options , count)
         
         return selected
+
+
